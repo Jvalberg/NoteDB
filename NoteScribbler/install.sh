1f760f7c11cb824a9491fea68ee5bddc8e98857f
JoakimValberg <joakim.valberg91@gmail.com>
2016-01-21 23:22:08 +1000

    Fixed minor bugs in NoteScribbler. This is wrong branch to do it. But i am tired.

--- NoteScribbler/install.sh
+++ NoteScribbler/install.sh
@@ -1,24 +1,28 @@
+# !/bin/bash
+# requires sudo
+
 BINARY_LOCATION=/home/jocke/dev/NoteDB/NoteScribbler
 
 # Generate the appropriate files and directories
-if [[ ! -d /tmp/NoteDB ]]; then
-	mkdir /tmp/NoteDB
+if [ ! -d /tmp/NoteDB ]; then
+	sudo -u user mkdir /tmp/NoteDB
 fi
 
-if [[ ! -f /tmp/NoteDB/note-blob.dat ]]; then
-	touch /tmp/NoteDB/note-blob.dat 
+if [ ! -f /tmp/NoteDB/note-blob.dat ]; then
+	sudo -u user touch /tmp/NoteDB/note-blob.dat 
 fi
 
-if [[ ! -d ~/.notedb ]]; then
-	mkdir ~/.notedb
+if [ ! -d /home/jocke/.notedb ]; then 
+	sudo -u user mkdir /home/jocke/.notedb
 fi
 
-if [[ ! -d ~/.notedb/blobs ]]; then
-	mkdir ~/.notedb/blobs
+if [ ! -d /home/jocke/.notedb/blobs ]; then
+	sudo -u user mkdir /home/jocke/.notedb/blobs
 fi
 
-if [[ -L /bin/NoteScribbler ]]; then
+if [ -L /bin/NoteScribbler ]; then
 	rm /bin/NoteScribbler
 fi
 
 ln -s "$BINARY_LOCATION/NoteScribbler" "/bin/NoteScribbler"
+hash NoteScribbler
